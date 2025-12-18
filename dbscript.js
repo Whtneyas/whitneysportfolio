5cd51be721ac7c8815682aef7e0b166d857f37a2
whtneyas <whitneyserwaa@gmail.com>
2025-12-18 20:40:19 +0000

    images for lofi sketch added

---
+++ dbscript.js
@@ -0,0 +1,15 @@
+
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+
+document.querySelectorAll('.zoomable').forEach(img => {
+    img.addEventListener('click', () => {
+        lightbox.style.display = 'flex';
+        lightboxImg.src = img.src;
+    });
+});
+
+lightbox.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+});
+
